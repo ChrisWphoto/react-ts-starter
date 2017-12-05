69714255795dace1e757fcc9c938ec984e4c14f1
chriswphoto <walter.chris@gmail.com>
2017-12-05 10:22:58 -0600

    more test

--- src/components/Hello.test.tsx
+++ src/components/Hello.test.tsx
@@ -8,7 +8,14 @@ Enzyme.configure({ adapter: new Adapter() });
 describe('A suite', function() {
   it('should render without throwing an error', function() {
     expect(
-      Enzyme.shallow(<Hello name="bob" enthusiasmLevel={1} />).contains(
+      Enzyme.shallow(<Hello name="bob" enthusiasmLevel={2} />).contains(
+        <div className="greeting">Hello bob!!</div>
+      )
+    ).toBe(true);
+  });
+  it('should render without throwing an error2', function() {
+    expect(
+      Enzyme.shallow(<Hello name="bob"/>).contains(
         <div className="greeting">Hello bob!</div>
       )
     ).toBe(true);
--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -2,10 +2,10 @@ import * as React from 'react';
 
 export interface Props {
   name: string;
-  enthusiasmLevel: number;
+  enthusiasmLevel?: number;
 }
 
-function Hello({ name, enthusiasmLevel }: Props) {
+function Hello({ name, enthusiasmLevel= 1 }: Props) {
   if (enthusiasmLevel <= 0) {
     throw new Error('You could be a little more enthusiastic. :D');
   }
